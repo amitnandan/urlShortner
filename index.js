51bd344fcfbf62fb1c8758c35c3ff6ab7b423a6b
amitnandan <a.amitnandan@gmail.com>
2023-09-21 16:16:22 +0530

    authentication Done

--- index.js
+++ index.js
@@ -1,79 +1,47 @@
 const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 const { connectToMongoDB } = require("./connect");
-const urlRoute = require("./router/url");
+const { restrictToLoggedinUserOnly, checkAuth } = require("./middleware/auth");
 const URL = require("./model/url");
-const path = require('path');
+
+const urlRoute = require("./router/url");
+const staticRoute = require("./router/staticRouter");
+const userRoute = require("./router/user");
+
 const app = express();
 const PORT = 8001;
 
-const staticRoute = require("./router/staticRouter")
-
-
-connectToMongoDB("mongodb://localhost:27017/short-url").then(() =>
+connectToMongoDB(process.env.MONGODB ?? "mongodb://localhost:27017/short-url").then(() =>
   console.log("Mongodb connected")
 );
-//telling that i am using a view engine
-app.set("view engine" ,"ejs");
-app.set('views', path.resolve('./view') );
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./view"));
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
+app.use("/url", restrictToLoggedinUserOnly, urlRoute);
+app.use("/user", userRoute);
+app.use("/", checkAuth ,  staticRoute);
 
-
-app.get('/test' , async(req,res)=>{
-
-  const allUrls = await URL.find({});
-  return res.render('home',{
-    urls:allUrls,
-  });
-}
-  )
-
-
-app.use('/url',urlRoute);
-
-
-
-
-app.use('/',staticRoute);
-
-
-
-
-
-
-
-
-app.get("url/:shortId", async (req, res) => {
-    const shortId = req.params.shortId;
-    const entry = await URL.findOneAndUpdate(
-      {
-        shortId,
-      },
-      {
-        $push: {
-          visitHistory: {
-            timestamp: Date.now(),
-          },
+app.get("/url/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
         },
-      }
-    );
-
-    if (!entry) {
-      // Handle the case where no entry was found for the given shortId
-      res.status(404).send("URL not found");
-      return;
+      },
     }
-  
-    // Check if the entry has a redirectURL
-    if (entry.redirectURL) {
-      res.redirect(entry.redirectURL);
-    } else {
-      res.status(500).send("Redirect URL not found in the entry");
-    }
-  });
-  
-
+  );
+  res.redirect(entry.redirectURL);
+});
 
 app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
